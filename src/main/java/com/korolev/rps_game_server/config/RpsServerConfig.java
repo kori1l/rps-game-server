d78705b4af4ebe7ac20e2af317c01be86d3608ba
Kirill Korolev <kori1l@yandex.ru>
2026-02-12 18:11:16 +0400

    fix tests start

--- src/main/java/com/korolev/rps_game_server/config/RpsServerConfig.java
+++ src/main/java/com/korolev/rps_game_server/config/RpsServerConfig.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +24,7 @@ public class RpsServerConfig {
     }
 
     @Bean
+    @ConditionalOnProperty(name = "rps.enabled", havingValue = "true", matchIfMissing = true)
     public CommandLineRunner run(NettyServer nettyServer, RpsChannelInitializer channelInitializer) {
         return args -> {
             nettyServer.start(channelInitializer);
--- src/test/java/com/korolev/rps_game_server/RpsGameServerApplicationTests.java
+++ src/test/java/com/korolev/rps_game_server/RpsGameServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.korolev.rps_game_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "rps.enabled=false")
 class RpsGameServerApplicationTests {
 
 	@Test
